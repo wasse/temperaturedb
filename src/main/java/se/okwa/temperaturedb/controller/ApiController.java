c428398d32ad103653e59886dec222d12fdcdb7e
Satomi <satoskit@users.noreply.github.com>
2019-12-17 14:15:05 +0100

    Re-delete the lines deleted by Mikel eralier.

--- src/main/java/se/okwa/temperaturedb/controller/ApiController.java
+++ src/main/java/se/okwa/temperaturedb/controller/ApiController.java
@@ -46,9 +46,6 @@ public class ApiController {
 		
 		logger.info(getValue(resp).getCity() + " AT " + getValue(resp).getTemperature());
 		
-//		WeatherReading reading = new WeatherReading(getValue(resp).getTemperature(), getValue(resp).getCity(), getValue(resp).getDate());
-//		readingRepository.save(reading);
-		
 		return resp;
 	}
 	
